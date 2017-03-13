47f12ccd27bc9f000c27091484a57ec159248bd8
Alex Chao <alex.h.chao@gmail.com>
2017-03-13 16:52:53 -0700

    JS is hard

---
+++ jsoop.js
@@ -0,0 +1,14 @@
+/**
+ * Various OOP styles I've encountered in JS.
+ */
+const JSOOP = (function() {
+
+    /* Prototypal */
+
+    /* Object Constructor */
+
+    /* ES6 Class */
+
+    return {};
+
+})();
