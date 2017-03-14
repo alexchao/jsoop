ef920fad942621cce3fdb7db85f4272ca382848a
Alex Chao <alex.h.chao@gmail.com>
2017-03-13 17:08:32 -0700

    prototypal

--- jsoop.js
+++ jsoop.js
@@ -3,7 +3,23 @@
  */
 const JSOOP = (function() {
 
-    /* Prototypal */
+    /**
+     * Prototypal
+     *
+     * - Constructor function has no return value
+     * - Private members are possible but not straightforward
+     */
+    const PrototypalCounter = function(initialValue) {
+        this._count = initialValue ? initialValue : 0;
+    };
+
+    PrototypalCounter.prototype.increment = function(delta) {
+        let d = typeof delta === 'undefined' ? 1 : delta;
+        this._count += delta;
+    };
+
+    PrototypalCounter.prototype.reset = function() { this._count = 0; };
+    PrototypalCounter.prototype.getCount = function() { return this._count; };
 
     /* Object Constructor */
 
